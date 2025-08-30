9d27701a9a7d02fed521514517b04be0d5097e63
Igor Dikiy <wapwiner@yandex.ru>
2025-08-30 22:22:35 +0300

    function mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from src import masks
+
+
+def mask_account_card(requisites_number: str) -> str:
+    """Принимает на вход строку формата: Visa Platinum 7000792289606361, или
+    Maestro 7000792289606361, или Счет 73654108430135874305. И возвращает
+    строку с замаскированным номером. Для карт и счетов используется разная маскировка"""
+    requisites_number_list = list(requisites_number)
+    requisites_str = ""
+    for number in requisites_number_list:
+        if number.isdigit():
+            requisites_str += number
+    if len(requisites_str) == 16:
+        return masks.get_mask_card_number(requisites_str)
+    elif len(requisites_str) == 20:
+        return masks.get_mask_account(requisites_str)
+    else:
+        return "Количество цифр номера карты(счёта) неверно."
+
+
+#if __name__ == '__main__':
+#    print(mask_account_card("Счет 73654108430135874305"))
+#    print(mask_account_card("Visa Platinum 7000792289606361"))
