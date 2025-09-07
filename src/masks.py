642ef891b9278bf6586fec82128d299844b02478
Igor Dikiy <wapwiner@yandex.ru>
2025-09-07 12:58:03 +0300

    update masks

--- src/masks.py
+++ src/masks.py
@@ -10,3 +10,7 @@ def get_mask_account(account: int) -> str:
     **XXXX"""
     account_str = str(account)
     return f"**{account_str[-4:]}"
+
+if __name__ == "__main__":
+    print(get_mask_account(73654108430135874305))
+    print(get_mask_card_number(7000792289606361))
