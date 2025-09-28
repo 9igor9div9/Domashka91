87dde5c650a4b15e0e5f220563e6bb6c8d56a0c7
Igor Dikiy <wapwiner@yandex.ru>
2025-09-28 19:53:32 +0300

    add filter_by_currency

---
+++ src/generators.py
@@ -0,0 +1,76 @@
+from typing import Iterator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            yield transaction
+
+
+
+
+transactions_1 =  [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2020-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "120000.07",
+              "currency": {
+                  "name": "RUR",
+                  "code": "RUR"
+              }
+          },
+          "description": "Перевод зарплаты",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2021-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "60000.93",
+                  "currency": {
+                      "name": "RUR",
+                      "code": "RUR"
+                  }
+              },
+              "description": "Перевод аванса",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       }]
+
+
+if __name__ == "__main__":
+    for _ in range(2):
+        print(next(filter_by_currency(transactions_1, "USD")))
